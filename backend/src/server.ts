c50087fc92efc98726dfd6e276f28067b955770c
Munfed Ali <alimunfed@Munfeds-MacBook-Air.local>
2025-10-14 12:57:10 -0700

    build: Add backend to App

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
