552b4ad8c863b60087b4b3d4e28b8e5aaed4d551
Munfed Ali <alimunfed@Munfeds-MacBook-Air.local>
2025-10-14 12:09:15 -0700

    feat: Add Header to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
-import './App.css';
 import Uploads from './pages/Uploads';
 import Reports from './pages/Reports';
+import Header from './components/Common/Header';
 
 export default function App() {
   return (
     <main>
+      <Header />
       <Routes>
         <Route path="/uploads" element={<Uploads />} />
         <Route path="/reports" element={<Reports />} />
