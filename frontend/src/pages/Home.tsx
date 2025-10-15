bd77e1e82393bf73fa1066272a8ef6345d270361
alimunfed17 <alimunfed17@gmail.com>
2025-10-15 13:46:53 -0700

    Update Home component layout and add backend notice
    Added a warning about backend startup time.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
     return (
-        <div className="flex flex-1 justify-center items-center">
-            <h3 className="font-semibold text-xl ">Navigate through the header links for /uploads & /reports</h3>
+        <div className="flex flex-1 flex-col justify-center items-center">
+            <h3 className="font-semibold text-xl">Navigate through the header links for /uploads & /reports</h3>
+            <h4 className="font-semibold text-lg text-red-600">The <i><strong>Backend</strong></i> is running on Render.com on free tier and it might take few minutes to get started.</h4>
         </div>
     );
 }
